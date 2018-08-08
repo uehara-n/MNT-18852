b14d9630ea1bd835234852b7a30b83fe5d3ed51d
uehara-n <na.ue0426@gmail.com>
2018-08-08 18:28:38 +0900

    ohuro完成

--- pc/ohuro.php
+++ pc/ohuro.php
@@ -1,7 +1,7 @@
 <div id="menu_ohuro">
-<p><img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/ohuro_main.png" alt="お風呂リフォーム" width="980" height="251" class="pb20" /></p>
-<p><img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_onayami.png" alt="お風呂リフォームこんな方におすすめ！" width="980" height="220" class="pb20" /></p>
-<p><img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_happy.png" alt="リフォームのご提案" width="100%" class="pb20" /></p>
+<p><img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/ohuro_main.png" alt="お風呂リフォーム" width="100%" class="pb40" /></p>
+<p class="tac"><img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_onayami.png" alt="お風呂リフォームこんな方におすすめ！" width="100%" class="pb40" /></p>
+<p><img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_happy.png" alt="リフォームのご提案" width="100%" class="pb40" /></p>
 
 <div class="box_flex">
   <img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit01_tit.png" alt="おそうじらくらく" width="100%" class="no_space"  />
@@ -9,12 +9,16 @@
     <div class="in_box">
       <h3 class="ohuroh3">カビ対策</h3>
     	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit01.jpg" alt="" width="100%" />
-    	<p></p>
+    	<p>
+        タテヨコに規則正しく刻み込まれたパターンで、表面の水を誘導。翌朝にはカラリと乾き、靴下のまま入っても大丈夫！乾きやすいから、カビにくい床です。また、床表面に特殊処理を処理た親水層の効果で、皮脂汚れと床の間に水が入り込み、汚れ落ちがスムーズに。ブラシでのお掃除がラクラクです。
+      </p>
     </div>
     <div class="in_box">
-      <h3 class="ohuroh3">カビ対策</h3>
-    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit022日.jpg" alt="" width="100%" />
-    	<p></p>
+      <h3 class="ohuroh3">排水口</h3>
+    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit02.jpg" alt="" width="100%" />
+    	<p>
+        特許取得しているリクシルのくるりんポイ排水口なら、浴槽の残り湯を利用して排水トラップ内にうず流れを発生させ、そのうずの力で排水口の汚れを付きにくくしてくれます。お湯を抜くたびに髪の毛やゴミをうずの力でくるくる～！シンプル構造でお掃除がもっと簡単に！
+      </p>
     </div>
   </div>
 </div>
@@ -25,16 +29,22 @@
   <div class="inner">
     <div class="in_box">
     	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit03.jpg" alt="" width="100%" />
-    	<p></p>
+    	<p>
+        お湯はり後、4時間以上たっても温かいと言われるTOTOの魔法びん浴槽。家族の入浴時間のズレをしっかりサポートしてくれます！「魔法びん浴槽」は、翌朝でもぬるま湯の状態で洗濯にもピッタリの温度！
+      </p>
     </div>
     <div class="in_box">
     	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit04.jpg" alt="" width="100%" />
-    	<p></p>
+    	<p>
+        サーモバスSなら温まったお湯が冷めにくい浴槽保温材と保温組フタの"ダブル保温"構造で、お湯を冷めにくく、4時間後でも温度が2.5℃しか下がらないので、入浴時間の異なるご家族でも時間を気にせず入浴できます。
+      </p>
     </div>
     <div class="in_box">
       <h3 class="ohuroh3">床暖房</h3>
     	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit05.jpg" alt="" width="100%" />
-    	<p></p>
+    	<p>
+        暖房、衣類乾燥、換気、涼風の機能を備えた「三冠王」入浴前にスイッチを入れておけば、高速温風が浴室をスピード暖房。冬の寒い日も快適で安心です。洗面暖房機を取り付けることで、冬場ヒヤリとする洗面所も洗面暖房機で温風暖房。大風量のドライヤーや扇風機としても活躍します。浴室と洗面所の温度差を抑え、お風呂上がりのヒヤッと感を解消します。
+      </p>
     </div>
   </div>
 </div>
@@ -45,45 +55,66 @@
   <div class="inner">
     <div class="in_box">
     	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit06.jpg" alt="" width="100%" />
-    	<p></p>
+      <h4>すべりにくい床へ</h4>
+      <p>昔ながらのタイル張りのお風呂より、足裏にフィットしてすべりにくい。お子様やご年配の方の転倒防止になる家族みんなの強い味方！ひざをついても痛くないクッション性にすぐれ、まるで畳のような柔らかい床です。</p>
     </div>
     <div class="in_box">
     	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit07.jpg" alt="" width="100%" />
-    	<p></p>
+      <h4>またぎやすい高さ</h4>
+      <p>浴槽の出入りを安全にしてくれます。</p>
     </div>
     <div class="in_box">
     	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit08.jpg" alt="" width="100%" />
-    	<p></p>
+      <h4>立ち座りをサポートする<br>「インテリアバー」</h4>
+      <p>立ち座り、浴槽の出入りをしっかりサポート。</p>
     </div>
     <div class="in_box">
     	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit09.jpg" alt="" width="100%" />
-    	<p></p>
+      <h4>ハンドクリップ</h4>
+      <p>身体を起こしやすく姿勢を保ちやすい</p>
     </div>
   </div>
 </div>
 
-<div class="box_flex">
-  <img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit10_tit.png" alt="" width="100%" class="no_space"  />
+<div class="box_usual">
+  <img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit04_tit.png" alt="" width="100%" class="no_space"  />
   <div class="inner">
     <div class="in_box">
-      <h3 class="ohuroh3">照明を工夫してムーディーに</h3>
-    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit11.jpg" alt="" width="100%" />
-    	<p></p>
+      <h3 class="ohuroh3"><span>照明を工夫してムーディーに</span></h3>
+    	<p><img class="leftb" src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit10.jpg" alt="" width="100%" /></p>
+    	<p class="txt">照明1つでガラリと雰囲気も変わります。<br>
+        ・天井のラインに沿わせるライン照明<br>
+        ・お湯ベールを照らす照明<br>
+        ・足元にキャンドルライトのようなほのかな光<br>
+        ・読書を楽しむスポットライト
+      </p>
     </div>
     <div class="in_box">
-      <h3 class="ohuroh3"></h3>
-    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit12.jpg" alt="" width="100%" />
-    	<p></p>
+      <h3 class="ohuroh3"><span>バスタブの形もいろいろ</span></h3>
+    	<p><img class="leftb" src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit11.jpg" alt="" width="100%" /></p>
+    	<p class="txt">いろいろなバスタブの形があるので、<br>
+        普通とはちょっと違うこだわりの<br>
+        お風呂にしたい方はぜひ！
+      </p>
     </div>
     <div class="in_box">
-      <h3 class="ohuroh3"></h3>
-    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit13.jpg" alt="" width="100%" />
-    	<p></p>
+      <h3 class="ohuroh3"><span>ゆっくりTVを見る！</span></h3>
+    	<p><img class="leftb" src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit12.jpg" alt="" width="100%" /></p>
+    	<p class="txt">バスルームで楽しむ映像とサウンドは格別。<br>
+        迫力の大画面をハイビジョンで楽しみたい方、<br>
+        臨場感にあふれた高音質を味わいたい方におすすめ♪<br>
+        防水スピーカーなどもご提案します。
+      </p>
     </div>
     <div class="in_box">
-      <h3 class="ohuroh3"></h3>
-    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit14.jpg" alt="" width="100%" />
-    	<p></p>
+      <h3 class="ohuroh3"><span>まるで温泉気分</span></h3>
+    	<p><img class="leftb" src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit13.jpg" alt="" width="100%" /></p>
+    	<p class="txt"><b>リクシル　スパージュ</b><br>
+        真のくつろぎのために、お風呂を愉しむ「お湯」の機能がたくさん♪<br>
+        ・首から肩に、やさしいお湯をまとうかのような新感覚の肩湯<br>
+        ・一流ホテルで浴びるような、たっぷりとしたオーバーヘッドシャワー<br>
+        ・マッサージ効果も期待できる打たせ湯他にもサイレントジェットなど、お風呂好きにはたまらないさまざまなワンランク上のバスルームを演出できます！
+      </p>
     </div>
   </div>
 </div>
