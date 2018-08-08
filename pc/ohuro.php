1599e3bef7bb31bda0121c8e8f659498e6984367
uehara-n <na.ue0426@gmail.com>
2018-08-08 17:54:57 +0900

    apc comp

---
+++ pc/ohuro.php
@@ -0,0 +1,115 @@
+<div id="menu_ohuro">
+<p><img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/ohuro_main.png" alt="お風呂リフォーム" width="980" height="251" class="pb20" /></p>
+<p><img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_onayami.png" alt="お風呂リフォームこんな方におすすめ！" width="980" height="220" class="pb20" /></p>
+<p><img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_happy.png" alt="リフォームのご提案" width="100%" class="pb20" /></p>
+
+<div class="box_flex">
+  <img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit01_tit.png" alt="おそうじらくらく" width="100%" class="no_space"  />
+	<div class="inner">
+    <div class="in_box">
+      <h3 class="ohuroh3">カビ対策</h3>
+    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit01.jpg" alt="" width="100%" />
+    	<p></p>
+    </div>
+    <div class="in_box">
+      <h3 class="ohuroh3">カビ対策</h3>
+    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit022日.jpg" alt="" width="100%" />
+    	<p></p>
+    </div>
+  </div>
+</div>
+
+<div class="box_flex">
+  <img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit02_tit.png" alt="" width="100%" class="no_space"  />
+  <h3 class="ohuroh3">浴槽の温度をキープ</h3>
+  <div class="inner">
+    <div class="in_box">
+    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit03.jpg" alt="" width="100%" />
+    	<p></p>
+    </div>
+    <div class="in_box">
+    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit04.jpg" alt="" width="100%" />
+    	<p></p>
+    </div>
+    <div class="in_box">
+      <h3 class="ohuroh3">床暖房</h3>
+    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit05.jpg" alt="" width="100%" />
+    	<p></p>
+    </div>
+  </div>
+</div>
+
+<div class="box_flex">
+  <img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit03_tit.png" alt="" width="100%" class="no_space"  />
+  <h3 class="ohuroh3">段差や深さの配慮</h3>
+  <div class="inner">
+    <div class="in_box">
+    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit06.jpg" alt="" width="100%" />
+    	<p></p>
+    </div>
+    <div class="in_box">
+    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit07.jpg" alt="" width="100%" />
+    	<p></p>
+    </div>
+    <div class="in_box">
+    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit08.jpg" alt="" width="100%" />
+    	<p></p>
+    </div>
+    <div class="in_box">
+    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit09.jpg" alt="" width="100%" />
+    	<p></p>
+    </div>
+  </div>
+</div>
+
+<div class="box_flex">
+  <img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit10_tit.png" alt="" width="100%" class="no_space"  />
+  <div class="inner">
+    <div class="in_box">
+      <h3 class="ohuroh3">照明を工夫してムーディーに</h3>
+    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit11.jpg" alt="" width="100%" />
+    	<p></p>
+    </div>
+    <div class="in_box">
+      <h3 class="ohuroh3"></h3>
+    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit12.jpg" alt="" width="100%" />
+    	<p></p>
+    </div>
+    <div class="in_box">
+      <h3 class="ohuroh3"></h3>
+    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit13.jpg" alt="" width="100%" />
+    	<p></p>
+    </div>
+    <div class="in_box">
+      <h3 class="ohuroh3"></h3>
+    	<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_merit14.jpg" alt="" width="100%" />
+    	<p></p>
+    </div>
+  </div>
+</div>
+
+
+<!-- 問い合わせ -->
+<div class="bnr_contact01_new">
+<a href="/contact"><img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/con_btn.png" alt="無料お見積りお問い合わせ" /></a>
+<br clear="all">
+</div>
+<!-- 問い合わせ -->
+
+<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_list_tit.png" alt="" width="980" height="492" class="no_space" />
+<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_list_l_01.png" alt="" width="980" height="308" class="no_space" />
+<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_list_l_02.png" alt="" width="980" height="308" class="no_space" />
+<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_list_t_01.png" alt="" width="980" height="308" class="no_space" />
+<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_list_t_02.png" alt="" width="980" height="309" class="no_space pb60" />
+
+
+<img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/ohuro/o_txt.png" alt="" width="980" height="150"  class="no_space pb20" />
+
+<!-- 問い合わせ -->
+<div class="bnr_contact01_new">
+<a href="/contact"><img src="<?php echo get_template_directory_uri(); ?>/page_image/menu/con_btn.png" alt="無料お見積りお問い合わせ" /></a>
+</div>
+<!-- 問い合わせ -->
+
+
+</div>
